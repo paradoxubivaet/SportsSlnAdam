0462624acd80bb10aa9b6815ca25af2468f3fd9a
ddossermi <paradox.ubivaet@mail.ru>
2022-09-13 18:53:11 +0800

    Добавьте файлы проекта.

---
+++ SportsStore/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        public long ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // Так как не все C# типы поддерживаются в SQL, этот
+        // атрибут гарантирует правильное соотношение типов
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ SportsStore/Startup.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection; 
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        private IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(opts =>
+            {
+                opts.UseSqlServer(
+                    Configuration["ConnectionStrings:SportsStoreConnection"]);
+            });
+            // Scoped - при каждом HTTP запросе создается свой собственный объект
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
+            services.AddScoped<IOrderRepository, EFOrderRepository>();
+            services.AddRazorPages();
+            services.AddDistributedMemoryCache();
+            services.AddSession();  
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("catpage",
+                                             "{category}/Page{productPage:int}",
+                                             new { Controller = "Home", Action = "Index" });
+
+                endpoints.MapControllerRoute("page", "Page{productPage:int}",
+                                             new { Controller = "Home", Action = "Index", productPage = 1});
+
+                endpoints.MapControllerRoute("category", "{category}",
+                                             new { Controller = "Home", Action = "Index", productPage = 1});
+
+                endpoints.MapControllerRoute("pagination",
+                                             "Products/Page{productPage}",
+                                             new { Controller = "Home", Action = "Index"});
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
+            });
+            SeedData.EnsurePopulated(app);
+        }
+    }
+}
